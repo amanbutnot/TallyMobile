2f79abec59a712b86b4a722256db613f573f3627
A-man404 <amansaini04a@gmail.com>
2025-09-18 22:14:11 +0530

    added sqldelight plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlin.serialization)
+    id("app.cash.sqldelight") version "2.1.0"
 
 }
 
