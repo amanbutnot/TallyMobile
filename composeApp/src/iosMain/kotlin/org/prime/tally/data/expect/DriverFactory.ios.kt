cf23b4330cfb927c02cf40456f857b5d498d2aa6
A-man404 <amansaini04a@gmail.com>
2025-09-28 23:05:43 +0530

    created ios actual function

--- composeApp/src/iosMain/kotlin/org/prime/tally/data/expect/DriverFactory.ios.kt
+++ composeApp/src/iosMain/kotlin/org/prime/tally/data/expect/DriverFactory.ios.kt
@@ -4,9 +4,50 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import org.prime.tally.data.utils.DB_FILE_NAME
 import org.tally.TallyDatabase
+import platform.Foundation.*
+import kotlinx.cinterop.*
 
 actual class DriverFactory {
+    @OptIn(ExperimentalForeignApi::class)
     actual fun createDriver(bytes: ByteArray): SqlDriver {
-        return NativeSqliteDriver(TallyDatabase.Schema, DB_FILE_NAME)
+        val fileManager = NSFileManager.defaultManager
+
+        // Get path to Documents directory
+        val urls = fileManager.URLsForDirectory(
+            directory = NSDocumentDirectory,
+            inDomains = NSUserDomainMask
+        )
+        val documentsDirectory = urls.firstOrNull() as? NSURL
+            ?: error("Unable to access Documents directory")
+
+        // Full path to database file
+        val dbUrl = documentsDirectory.URLByAppendingPathComponent(DB_FILE_NAME)
+            ?: error("Unable to create DB URL")
+
+        val dbPath = dbUrl.path ?: error("Unable to resolve DB path")
+
+        // Remove old DB if exists
+        if (fileManager.fileExistsAtPath(dbPath)) {
+            fileManager.removeItemAtPath(dbPath, null)
+        }
+
+        // Write new DB file
+        val nsData = bytes.toNSData()
+        nsData.writeToFile(dbPath, atomically = true)
+
+        // Create SQLDelight driver with the file path
+        return NativeSqliteDriver(
+            schema = TallyDatabase.Schema,
+            name = dbPath
+        )
     }
+}
+
+// Helper: convert ByteArray -> NSData safely
+@OptIn(ExperimentalForeignApi::class)
+private fun ByteArray.toNSData(): NSData = memScoped {
+    NSData.create(
+        bytes = this@toNSData.refTo(0).getPointer(this),
+        length = size.toULong()
+    )
 }
