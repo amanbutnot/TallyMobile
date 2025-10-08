c003a23b956edfe9458edb40da95031fa819aece
A-man404 <amansaini04a@gmail.com>
2025-10-08 20:38:36 +0530

    created common date format function

---
+++ composeApp/src/commonMain/kotlin/org/prime/tally/ui/shared/globalShared/Tdate.kt
@@ -0,0 +1,28 @@
+package org.prime.tally.ui.shared.globalShared
+
+fun Tdate(date: String): String {
+    val (yy, mm, dd) = date.split("-")
+    //pass the saved date format in this
+    val dateFormat = "dd-mm-yyyy"
+    return when (dateFormat) {
+        "dd-mm-yyyy" -> {
+            "$dd-$mm-$yy"
+        }
+
+        "yyyy-mm-dd" -> {
+            "$yy-$mm-$dd"
+        }
+
+        "yyyy-dd-mm" -> {
+            "$yy-$dd-$mm"
+        }
+
+        "mm-dd-yyyy" -> {
+            "$mm-$dd-$yy"
+        }
+
+        else -> {
+            "$dd-$mm-$yy"
+        }
+    }
+}
