f7a9f882383798f26488c013af66419183096b6c
A-man404 <amansaini04a@gmail.com>
2025-12-19 10:52:40 +0530

    added inventory voucher create, edit, list, and delete apis

--- composeApp/src/commonMain/kotlin/org/prime/tally/business/viewmodel/transactions/InventoryVoucherViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/prime/tally/business/viewmodel/transactions/InventoryVoucherViewModel.kt
@@ -5,7 +5,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import org.prime.tally.business.repository.transactions.DeleteResponse
 import org.prime.tally.business.repository.transactions.InventoryVoucherRepo
+import org.prime.tally.data.model.transactions.InventoryItemResponse
+import org.prime.tally.data.model.transactions.InventoryListRequest
+import org.prime.tally.data.model.transactions.InventoryListResponse
 import org.prime.tally.data.model.transactions.InventoryVoucherRequest
 import org.prime.tally.data.model.transactions.InventoryVoucherResponse
 
@@ -14,13 +18,28 @@ class InventoryVoucherViewModel : ViewModel() {
     private val _dataState = mutableStateOf(DataState<InventoryVoucherResponse>())
     val dataState: State<DataState<InventoryVoucherResponse>> = _dataState
 
+    private val _oneState = mutableStateOf(DataState<InventoryItemResponse>())
+    val oneState: State<DataState<InventoryItemResponse>> = _oneState
+    private val _deleteState = mutableStateOf(DataState<DeleteResponse>())
+    val deleteState: State<DataState<DeleteResponse>> = _deleteState
 
-    fun createInventoryVoucher(inventoryVoucherRequest: InventoryVoucherRequest, onSuccess: () -> Unit) {
+    private val _listState = mutableStateOf(DataState<List<InventoryListResponse>>())
+    val listState: State<DataState<List<InventoryListResponse>>> = _listState
+
+
+    fun createEditInventoryResponse(
+        inventoryVoucherRequest: InventoryVoucherRequest,
+        onSuccess: () -> Unit, url: String
+    ) {
         viewModelScope.launch {
 
             _dataState.value = DataState(isLoading = true)
 
-            val res = InventoryVoucherRepo.createInventoryVch(inventoryVoucherRequest)
+            println("CreateEditRequest: $inventoryVoucherRequest")
+            val res = InventoryVoucherRepo.createInventoryVch(
+                inventoryVoucherRequest = inventoryVoucherRequest,
+                endpoint = url
+            )
 
             if (res?.statuscode == 200) {
                 _dataState.value = DataState(
@@ -32,6 +51,86 @@ class InventoryVoucherViewModel : ViewModel() {
                 onSuccess()
             } else {
                 _dataState.value = DataState(
+                    success = false,
+                    isLoading = false,
+                    error = res?.message ?: "Error Occurred: Please try again."
+                )
+            }
+        }
+    }
+
+    fun deleteInventoryVch(
+        tranId: Int, vchType: Int,
+
+    ) {
+        viewModelScope.launch {
+
+            _deleteState.value = DataState(isLoading = true)
+
+            val res = InventoryVoucherRepo.deleteInventoryVoucher(
+                tranId = tranId,
+                vchType = vchType
+            )
+
+            if (res?.statuscode == 200) {
+                _deleteState.value = DataState(
+                    success = true,
+                    isLoading = false,
+                    data = res.data,
+                    message = res.message
+                )
+
+            } else {
+                _deleteState.value = DataState(
+                    success = false,
+                    isLoading = false,
+                    error = res?.message ?: "Error Occurred: Please try again."
+                )
+            }
+        }
+    }
+
+
+    fun getOneInventoryVoucher(tranId: Int) {
+        viewModelScope.launch {
+
+            _oneState.value = DataState(isLoading = true)
+
+            val res = InventoryVoucherRepo.getInventoryVoucher(tranId)
+
+            if (res?.statuscode == 200) {
+                _oneState.value = DataState(
+                    success = true,
+                    isLoading = false,
+                    data = res.data,
+                    message = res.message
+                )
+            } else {
+                _oneState.value = DataState(
+                    success = false,
+                    isLoading = false,
+                    error = res?.message ?: "Error Occurred: Please try again."
+                )
+            }
+        }
+    }
+
+    fun listInventoryVch(inventoryListRequest: InventoryListRequest) {
+        viewModelScope.launch {
+
+            _listState.value = DataState(isLoading = true)
+
+            val res = InventoryVoucherRepo.listInventoryVch(inventoryListRequest)
+
+            if (res?.statuscode == 200) {
+                _listState.value = DataState(
+                    success = true,
+                    isLoading = false,
+                    data = res.data,
+                    message = res.message
+                )
+            } else {
+                _listState.value = DataState(
                     success = false,
                     isLoading = false,
                     error = res?.message ?: "Error Occurred: Please try again."
--- composeApp/src/commonMain/kotlin/org/prime/tally/data/model/transactions/InventoryVoucherRequest.kt
+++ composeApp/src/commonMain/kotlin/org/prime/tally/data/model/transactions/InventoryVoucherRequest.kt
@@ -1,21 +1,30 @@
 package org.prime.tally.data.model.transactions
 
 import kotlinx.serialization.Serializable
-import org.prime.tally.ui.screen.transactions.sale.SundryItem
 
 @Serializable
 data class InventoryVoucherRequest(
     val billing_guid: String,
     val vch_type: Int,
     val billing_name: String,
+    val TranDate: String,
     val billing_mobile: String,
     val billing_state: String,
     val billing_country: String,
     val billing_address: String,
     val taxType: Int,
     val items: List<BillingItem>,
-    val sundries: List<SundryItem>
+    val sundries: List<SundryItem>,
+    val Narration: String,
+    val TransactionID:Int?=null
 )
+
+@Serializable
+data class SundryItem(
+    val name: String,
+    val amount: Double = 0.0
+)
+
 
 @Serializable
 data class BillingItem(
@@ -26,11 +35,93 @@ data class BillingItem(
     val discount_percent: Double? = null,
     val discount_amt: Double? = null,
     val tax_rate1: Double,
-    val tax_rate2: Double
+    val tax_rate2: Double,
+    val taxable:Double,
+    val gstAmt: Double,
+    val net:Double
 )
 
 
 @Serializable
 data class InventoryVoucherResponse(
     val VoucherNumber: String
+)
+
+@Serializable
+data class InventoryListResponse(
+    val id: Int,
+    val store_id: Int,
+    val user_id: Int,
+    val VchType: Int,
+    val order_no: String,
+    val total_amount: String,
+    val status: String,
+    val status_billed: String,
+    val billing_guid: String,
+    val billing_name: String,
+    val billing_mobile: String,
+    val billing_address: String,
+    val billing_state: String,
+    val billing_country: String,
+    val created_at: String,
+    val razorpay_payment_id: String,
+    val Others1: String,
+    val Others2: String
+)
+
+@Serializable
+data class InventoryListRequest(
+    val VchType: Int,
+    val StartDate: String,
+    val EndDate: String
+)
+
+@Serializable
+data class InventoryItemResponse(
+    val id: Int,
+    val store_id: Int,
+    val user_id: Int,
+    val VchType: Int,
+    val AutoVchNo: Int,
+    val order_no: String,
+    val total_amount: String,
+    val status: String,
+    val status_billed: String,
+    val billing_guid: String,
+    val billing_name: String,
+    val billing_mobile: String,
+    val billing_address: String,
+    val billing_state: String,
+    val billing_country: String,
+    val created_at: String,
+    val razorpay_payment_id: String,
+    val Others1: String,
+    val Others2: String,
+    val items: List<Item>, val taxType: Int
+)
+
+@Serializable
+data class Item(
+    val id: Int,
+    val store_id: Int,
+    val order_id: Int,
+    val product_id: String,
+    val product_name: String,
+    val quantity: Int,
+    val price: String,
+    val list_price: String,
+    val discount_percent: String,
+    val discount_amt: String,
+    val nett_price: String,
+    //taxable
+    val item_amount: String,
+    //gst
+    val tax_rate1: String,
+    val tax_rate2: String,
+    //gst amount
+    val taxamt1: String,
+    val taxamt2: String,
+    //net
+    val total_amt: String,
+    val taxType: Int
 )
