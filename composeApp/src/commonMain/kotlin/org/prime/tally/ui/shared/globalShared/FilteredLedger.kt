ffc57d6eee1255fc714545341ef42c6e1db45d53
A-man404 <amansaini04a@gmail.com>
2025-12-31 23:37:55 +0530

    added getItemMasters function

--- composeApp/src/commonMain/kotlin/org/prime/tally/ui/shared/globalShared/FilteredLedger.kt
+++ composeApp/src/commonMain/kotlin/org/prime/tally/ui/shared/globalShared/FilteredLedger.kt
@@ -3,6 +3,7 @@ package org.prime.tally.ui.shared.globalShared
 import org.prime.tally.data.expect.DatabaseHolder
 import org.prime.tally.data.utils.SharedPrefs
 import org.tally.LedgerMaster
+import org.tally.Products
 import org.tally.TallyDatabase
 
 // Extension functions
@@ -24,10 +25,11 @@ fun getLedgerMasters(db: TallyDatabase): List<LedgerMaster> {
     val filterAGRP = perms?.FilterAGRP == "Y"
     val filterAccounts = perms?.FilterAccounts == "Y"
 
-
-    println("AGRP: $filterAGRP and filter accounts $filterAccounts")
-
     return when {
+        perms==null->{
+            db.ledgerMasterQueries.selectAll()
+                .executeAsList()
+        }
         // Both filters active
         filterAGRP && filterAccounts -> {
             val groupCodes = perms.ConfigAGRP.parseToDoubleList()
@@ -53,6 +55,49 @@ fun getLedgerMasters(db: TallyDatabase): List<LedgerMaster> {
         // No filters
         else -> {
             db.ledgerMasterQueries.selectAll()
+                .executeAsList()
+        }
+    }
+}
+
+fun getItemMasters(db: TallyDatabase): List<Products> {
+    val perms = SharedPrefs.Permissions.get()
+    val filterAGRP = perms?.FilterIGRP == "Y"
+    val filterAccounts = perms?.FilterItems == "Y"
+
+    println(filterAccounts)
+    println(filterAGRP)
+
+    return when {
+        perms==null->{
+            db.productsQueries.selectAll()
+                .executeAsList()
+        }
+        // Both filters active
+        filterAGRP && filterAccounts -> {
+            val groupCodes = perms.FilterIGRP.parseToDoubleList()
+            val excludeGuids = perms.FilterItems.parseToStringList()
+            db.productsQueries.selectAllFilterAGRP(groupCodes, excludeGuids)
+                .executeAsList()
+        }
+
+        // Only GroupCode filter
+        filterAGRP -> {
+            val groupCodes = perms.FilterIGRP.parseToDoubleList()
+            db.productsQueries.selectByGroupCode(groupCodes)
+                .executeAsList()
+        }
+
+        // Only GUID exclusion
+        filterAccounts -> {
+            val excludeGuids = perms.FilterItems.parseToStringList()
+            db.productsQueries.selectExcludingGuid(excludeGuids)
+                .executeAsList()
+        }
+
+        // No filters
+        else -> {
+            db.productsQueries.selectAll()
                 .executeAsList()
         }
     }
