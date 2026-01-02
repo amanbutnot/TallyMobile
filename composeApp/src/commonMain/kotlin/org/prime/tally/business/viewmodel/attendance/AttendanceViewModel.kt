6949664bf182618657f57e6339b3103da092dcd9
A-man404 <amansaini04a@gmail.com>
2026-01-02 22:55:54 +0530

    added get salesman list query

--- composeApp/src/commonMain/kotlin/org/prime/tally/business/viewmodel/attendance/AttendanceViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/prime/tally/business/viewmodel/attendance/AttendanceViewModel.kt
@@ -10,12 +10,15 @@ import org.prime.tally.data.model.attendance.AttendanceListRequest
 import org.prime.tally.data.model.attendance.AttendanceListResponse
 import org.prime.tally.data.model.attendance.AttendanceRequest
 import org.prime.tally.data.model.attendance.AttendanceResponse
+import org.prime.tally.data.model.attendance.SalesmanList
 
 class AttendanceViewModel : ViewModel() {
     private val _dataState = mutableStateOf(DataState<AttendanceResponse>())
     val dataState: State<DataState<AttendanceResponse>> = _dataState
     private val _listState = mutableStateOf(DataState<List<AttendanceListResponse>>())
     val listState: State<DataState<List<AttendanceListResponse>>> = _listState
+    private val _salesmanList = mutableStateOf(DataState<List<SalesmanList>>())
+    val salesmanList: State<DataState<List<SalesmanList>>> = _salesmanList
 
     fun sendAttendance(attendanceRequest: AttendanceRequest, onSuccess: () -> Unit) {
         viewModelScope.launch {
@@ -48,10 +51,31 @@ class AttendanceViewModel : ViewModel() {
 
             if (res?.statuscode == 200) {
                 _listState.value = DataState(
-                    success = true, message = res.message, isLoading = false,data = res.data
+                    success = true, message = res.message, isLoading = false, data = res.data
                 )
             } else {
                 _listState.value = DataState(
+                    success = false,
+                    isLoading = false,
+                    error = res?.message ?: "Error Occurred. Please try again."
+                )
+            }
+        }
+    }
+
+    fun getSalesmanList() {
+        viewModelScope.launch {
+
+            _salesmanList.value = DataState(isLoading = true)
+
+            val res = AttendanceRepository.getSalesmanList()
+
+            if (res?.statuscode == 200) {
+                _salesmanList.value = DataState(
+                    success = true, message = res.message, isLoading = false, data = res.data
+                )
+            } else {
+                _salesmanList.value = DataState(
                     success = false,
                     isLoading = false,
                     error = res?.message ?: "Error Occurred. Please try again."
