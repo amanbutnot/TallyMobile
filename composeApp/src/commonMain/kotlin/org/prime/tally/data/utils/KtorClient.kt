ec157d7908a16782cb92ecb5f8fdf39714d931b4
A-man404 <amansaini04a@gmail.com>
2026-01-06 22:18:14 +0530

    added timeout to ktor client

--- composeApp/src/commonMain/kotlin/org/prime/tally/data/utils/KtorClient.kt
+++ composeApp/src/commonMain/kotlin/org/prime/tally/data/utils/KtorClient.kt
@@ -1,6 +1,7 @@
 package org.prime.tally.data.utils
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -13,6 +14,11 @@ object KtorClient {
                 isLenient = true
                 ignoreUnknownKeys = true
             })
+        }
+        install(HttpTimeout){
+            requestTimeoutMillis = 60_000    // total request time
+            connectTimeoutMillis = 30_000    // connection phase
+            socketTimeoutMillis = 60_000     // waiting for data
         }
 
 
