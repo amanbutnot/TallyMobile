2fc984736a08deb316cae56681b67ae65c6cba0c
A-man404 <amansaini04a@gmail.com>
2026-01-09 22:48:08 +0530

    added product parameter report screen

---
+++ composeApp/src/commonMain/kotlin/org/prime/tally/ui/screen/reports/stock/ParameterReportScreen.kt
@@ -0,0 +1,273 @@
+package org.prime.tally.ui.screen.reports.stock
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.style.TextAlign
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import kotlinx.coroutines.launch
+import org.prime.tally.data.expect.DatabaseHolder
+import org.prime.tally.data.expect.formatToAmtDec
+import org.prime.tally.data.expect.formatToQtyDec
+import org.prime.tally.ui.printing.Quadruple
+import org.prime.tally.ui.printing.fourHeaderHtml
+import org.prime.tally.ui.printing.threeHeaderHtml
+import org.prime.tally.ui.screen.reports.productReport.ProductReportScreen
+import org.prime.tally.ui.shared.composables.MenuItemData
+import org.prime.tally.ui.shared.composables.TallyCircularLoader
+import org.prime.tally.ui.shared.composables.TallyLoadingDialog
+import org.prime.tally.ui.shared.composables.TallyReportScaffold
+import org.prime.tally.ui.shared.composables.TallySearchBar
+import org.prime.tally.ui.shared.globalShared.getProductParamStockItems
+import org.prime.tally.ui.shared.globalShared.getProductStockItems
+import org.prime.tally.ui.shared.reportsShared.PdfAction
+import org.prime.tally.ui.shared.reportsShared.ReportColumn
+import org.prime.tally.ui.shared.reportsShared.TableCell
+import org.prime.tally.ui.shared.reportsShared.TallyReportBottomBar
+import org.prime.tally.ui.shared.reportsShared.TallyReportHeaderCard
+import org.prime.tally.ui.shared.reportsShared.TallyReportLazyList
+import org.prime.tally.ui.shared.reportsShared.handlePdfAction
+import org.tally.GetProductParamStockList
+import org.tally.GetProductStockItemList
+import kotlin.math.absoluteValue
+
+object ParameterReportScreen : Screen {
+    @Composable
+    override fun Content() {
+
+        val db = DatabaseHolder.instance
+
+        var list by remember { mutableStateOf<List<GetProductParamStockList>>(emptyList()) }
+        var isLoading by remember { mutableStateOf(true) }
+        var showSearchBar by remember { mutableStateOf(false) }
+        var searchQuery by remember { mutableStateOf("") }
+        val focusRequester = remember { FocusRequester() }
+        val nav = LocalNavigator.currentOrThrow
+        var shareLoading by remember { mutableStateOf(false) }
+        val scope = rememberCoroutineScope()
+
+
+        val column1Weight = 0.5f
+        val column2Weight = 0.2f
+        val column3Weight = 0.2f
+        val column4Weight = 0.4f
+
+        val totalQty = list.sumOf { it.mvalue1?.toDouble() ?: 0.0 }
+        val totalAmt = list.sumOf { it.mvalue2?.toDouble() ?: 0.0 }
+
+        LaunchedEffect(Unit) {
+            isLoading = true
+            list = getProductParamStockItems(db)
+
+            isLoading = false
+
+
+        }
+        LaunchedEffect(showSearchBar) {
+            if (showSearchBar) {
+                focusRequester.requestFocus()
+            }
+        }
+        val filteredList = if (searchQuery.isEmpty()) {
+            list
+        } else {
+            list.filter {
+                it.ProductName?.startsWith(
+                    searchQuery,
+                    ignoreCase = true
+                ) == true
+            }
+        }
+
+        val rows: List<Triple< String, String, String>> = filteredList.map { item ->
+            Triple(
+                item.ProductName ?: "",
+                item.mvalue1?.formatToQtyDec() ?: "-",
+                item.mvalue2?.formatToAmtDec() ?: ""
+            )
+        }
+
+        val menuItems = listOf(
+            MenuItemData(
+                title = "Download",
+                icon = Icons.Default.Download,
+                onClick = {
+                    scope.launch {
+                        handlePdfAction(
+                            fileName = "Parameter",
+                            htmlContent = threeHeaderHtml(
+                                title = "Parameter",
+                                headers = Triple("Item Name", "Qty", "Amount"),
+                                rows = rows,
+                                totalDebit = totalQty.formatToQtyDec().toDouble(),
+                                totalCredit = totalAmt.formatToAmtDec().toDouble(),
+                                date = ""
+                            ),
+                            action = PdfAction.Download,
+                            onLoadingChange = { shareLoading = it }
+                        )
+                    }
+                }
+            ),
+            MenuItemData(
+                title = "Share",
+                icon = Icons.Default.Share,
+                onClick = {
+                    scope.launch {
+                        handlePdfAction(
+                            fileName = "Parameter",
+                            htmlContent = threeHeaderHtml(
+                                title = "Parameter",
+                                headers = Triple("Item Name", "Qty", "Amount"),
+                                rows = rows,
+                                totalDebit = totalQty.formatToQtyDec().toDouble(),
+                                totalCredit = totalAmt.formatToAmtDec().toDouble(),
+                                date = ""
+                            ),
+                            action = PdfAction.Download,
+                            onLoadingChange = { shareLoading = it }
+                        )
+                    }
+                }
+            )
+        )
+        if (shareLoading) {
+            TallyLoadingDialog("Generating Report")
+        }
+
+
+        TallyReportScaffold(
+            "Parameter Report", showBottomBar = true,
+            showSearchAction = true,
+            showBurgerMenu = true,
+            menuItems = menuItems,
+            onSearchClick = { showSearchBar = !showSearchBar },
+            bottomBarContent = {
+                TallyReportBottomBar(
+                    columns = listOf(
+                        ReportColumn(
+                            "Rows: ${filteredList.count()}",
+                            column1Weight,
+                            TextAlign.Start
+                        ),
+                        ReportColumn(
+                            totalQty.absoluteValue.formatToQtyDec(),
+                            column2Weight,
+                            TextAlign.End
+                        ),
+                        ReportColumn(
+                            totalAmt.absoluteValue.formatToAmtDec(),
+                            column3Weight,
+                            TextAlign.End
+                        )
+                    ),
+                )
+            },
+            content = { paddingValues ->
+                if (isLoading) {
+                    Box(
+                        Modifier.Companion.fillMaxSize(),
+                        contentAlignment = Alignment.Companion.Center
+                    ) {
+                        TallyCircularLoader()
+                    }
+                } else {
+                    Column(modifier = Modifier.Companion.fillMaxSize().padding(paddingValues)) {
+                        if (showSearchBar) {
+                            TallySearchBar(
+                                searchQuery = searchQuery,
+                                onQueryChange = { searchQuery = it },
+                                modifier = Modifier.Companion.focusRequester(focusRequester)
+                            )
+                        }
+                        TallyReportHeaderCard(
+                            columns = listOf(
+                                ReportColumn(
+                                    "Item Name",
+                                    column1Weight,
+                                    TextAlign.Start
+                                ),
+//                                ReportColumn(
+//                                    "Unit",
+//                                    column2Weight,
+//                                    TextAlign.End
+//                                ),
+                                ReportColumn(
+                                    "Qty",
+                                    column3Weight,
+                                    TextAlign.End
+                                ),
+                                ReportColumn(
+                                    "Amount",
+                                    column4Weight,
+                                    TextAlign.End
+                                )
+                            )
+                        )
+
+                        TallyReportLazyList(
+                            items = filteredList,
+                            onItemClick = { item ->
+                                //TODO: this is the StockItemReportListScreen
+                                //   nav.push(StockItemReportScreen(item.Item_Name))
+                                println(item.MasterCode1?.toInt())
+                                nav.push(
+                                    ProductReportScreen(
+                                        item.MasterCode1?.toInt().toString(),
+                                        isMain = false
+                                    )
+                                )
+
+                            },
+
+                            content = { item ->
+                                TableCell(
+                                    text = item.ProductName ?: "",
+                                    weight = column1Weight,
+                                    textAlign = TextAlign.Companion.Start,
+                                    isHeader = false
+                                )
+                                //TODO: ENABLE LATER
+//
+//                                TableCell(
+//                                    text = item.Item_Unit.toString(),
+//                                    weight = column2Weight,
+//                                    textAlign = TextAlign.Companion.End,
+//                                    isHeader = false
+//                                )
+                                TableCell(
+                                    text = item.mvalue1?.formatToQtyDec() ?: "-",
+                                    weight = column3Weight,
+                                    textAlign = TextAlign.Companion.End,
+                                    isHeader = false
+                                )
+                                TableCell(
+                                    text = "-",
+                                    weight = column4Weight,
+                                    textAlign = TextAlign.Companion.End,
+                                    isHeader = false
+                                )
+                            }
+                        )
+                    }
+                }
+            })
+    }
+}
