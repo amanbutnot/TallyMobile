e4e904c5a9adc68fe6be36e058dfc11c6d18e76d
A-man404 <amansaini04a@gmail.com>
2026-01-12 23:41:46 +0530

    improved product search in stock report screen

--- composeApp/src/commonMain/kotlin/org/prime/tally/ui/screen/reports/stock/StockReportScreen.kt
+++ composeApp/src/commonMain/kotlin/org/prime/tally/ui/screen/reports/stock/StockReportScreen.kt
@@ -96,12 +96,17 @@ object StockReportScreen : Screen {
         val filteredList = if (searchQuery.isEmpty()) {
             list
         } else {
-            list.filter {
-                it.ProductName?.startsWith(
-                    searchQuery,
-                    ignoreCase = true
-                ) == true
+            val startsWith = list.filter {
+                it.ProductName?.startsWith(searchQuery, ignoreCase = true) == true
             }
+
+            val contains = list.filter {
+                val value = it.ProductName
+                value?.contains(searchQuery, ignoreCase = true) == true &&
+                        value?.startsWith(searchQuery, ignoreCase = true) == false
+            }
+
+            startsWith + contains
         }
 
         val rows: List<Quadruple<String, String, String, String>> = filteredList.map { item ->
