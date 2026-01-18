7dd93c03d84035290dee55ab82040a1beb188e5c
A-man404 <amansaini04a@gmail.com>
2026-01-18 23:03:11 +0530

    updated mobile version to 6

--- composeApp/src/commonMain/kotlin/org/prime/tally/data/utils/CONSTANTS.kt
+++ composeApp/src/commonMain/kotlin/org/prime/tally/data/utils/CONSTANTS.kt
@@ -3,4 +3,4 @@ package org.prime.tally.data.utils
 const val BASE_URL = "https://saurabhahuja.com/TallyConnect"
 const val DB_FILE_NAME = "tallydatabase.db"
 
-const val MOBILE_VERSION = 5
+const val MOBILE_VERSION = 6
