baa5e0ab4e376373ddb154c2ffd355f0c8cbf975
A-man404 <amansaini04a@gmail.com>
2026-02-11 23:04:14 +0530

    updated version

--- composeApp/src/commonMain/kotlin/org/prime/easykarobar/data/utils/CONSTANTS.kt
+++ composeApp/src/commonMain/kotlin/org/prime/easykarobar/data/utils/CONSTANTS.kt
@@ -3,4 +3,4 @@ package org.prime.easykarobar.data.utils
 const val BASE_URL = "https://saurabhahuja.com/TallyConnect"
 const val DB_FILE_NAME = "tallydatabase.db"
 
-const val MOBILE_VERSION = 18
+const val MOBILE_VERSION = 19
