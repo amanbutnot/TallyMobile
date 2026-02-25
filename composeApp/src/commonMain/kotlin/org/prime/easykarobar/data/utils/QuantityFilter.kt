8db96bdcd7891c692f4e24a135832d459b971567
A-man404 <amansaini04a@gmail.com>
2026-02-25 23:57:15 +0530

    feat: Add group and exclusion filters to godown-wise stock queries
    - Enhance `godownWiseClosingStockList` and `godownWiseOnEnterList` SQL queries to allow filtering by stock group and excluding specific items.
    - Update `showQtyToSalesman` and `showAmtToSalesman` utility functions to incorporate the `FilterMobile` permission, ensuring quantity and amount visibility is correctly handled based on user permissions.

--- composeApp/src/commonMain/kotlin/org/prime/easykarobar/data/utils/QuantityFilter.kt
+++ composeApp/src/commonMain/kotlin/org/prime/easykarobar/data/utils/QuantityFilter.kt
@@ -1,8 +1,11 @@
 package org.prime.easykarobar.data.utils
 
-fun showQtyToSalesman():Boolean{
-    return if (SharedPrefs.Permissions.get()?.FilterQty == "False" || SharedPrefs.Permissions.get()?.FilterQty == null) true else false
+fun showQtyToSalesman(): Boolean {
+    val permissions = SharedPrefs.Permissions.get()
+    return if (permissions?.FilterMobile == "N" || permissions == null || SharedPrefs.Permissions.get()?.FilterQty == "False" || SharedPrefs.Permissions.get()?.FilterQty == null) true else false
 }
-fun showAmtToSalesman():Boolean{
-    return if (SharedPrefs.Permissions.get()?.FilterAmount == "False" || SharedPrefs.Permissions.get()?.FilterAmount == null) true else false
+
+fun showAmtToSalesman(): Boolean {
+    val permissions = SharedPrefs.Permissions.get()
+    return if (permissions?.FilterMobile == "N" || permissions == null || SharedPrefs.Permissions.get()?.FilterAmount == "False" || SharedPrefs.Permissions.get()?.FilterAmount == null) true else false
 }
