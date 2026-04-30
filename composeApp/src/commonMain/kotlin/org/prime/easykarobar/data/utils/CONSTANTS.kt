54f358a289f07b34f9d14a1558e2f6b083a6fc56
A-man404 <amansaini04a@gmail.com>
2026-04-30 23:05:34 +0530

    changed constant number

--- composeApp/src/commonMain/kotlin/org/prime/easykarobar/data/utils/CONSTANTS.kt
+++ composeApp/src/commonMain/kotlin/org/prime/easykarobar/data/utils/CONSTANTS.kt
@@ -3,4 +3,4 @@ package org.prime.easykarobar.data.utils
 const val BASE_URL = "https://easykarobar.in/EasyConnect"
 const val DB_FILE_NAME = "tallydatabase.db"
 
-const val MOBILE_VERSION =60
+const val MOBILE_VERSION =62
