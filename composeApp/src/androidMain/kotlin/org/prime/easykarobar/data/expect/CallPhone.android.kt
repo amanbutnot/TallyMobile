d5bbd1aa40e67b83094c3c97e29836e1808a4780
A-man404 <amansaini04a@gmail.com>
2026-08-05 22:08:11 +0530

    added +91

--- composeApp/src/androidMain/kotlin/org/prime/easykarobar/data/expect/CallPhone.android.kt
+++ composeApp/src/androidMain/kotlin/org/prime/easykarobar/data/expect/CallPhone.android.kt
@@ -5,7 +5,7 @@ import android.net.Uri
 import org.prime.easykarobar.AppContextHolder
 
 actual fun callPhone(number: String) {
-    val uri = Uri.parse("tel:91${number}")
+    val uri = Uri.parse("tel:+91${number}")
     val intent = Intent(Intent.ACTION_DIAL, uri).apply {
         addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     }
